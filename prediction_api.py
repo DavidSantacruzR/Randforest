57eee3063e90054ebb0cfb40e709e455d36a2845
David <axelsantacruzr@gmail.com>
2022-02-03 16:38:52 -0500

    Changed folder structure, imports and refactored code to make it simpler.

--- prediction_api.py
+++ prediction_api.py
@@ -1,42 +1,27 @@
-from utils import data_preprocessing
-from mlClassifier.dependencies.logistic_model_preprocessing import ModelPreProcessing, ModelScalingAndEncoding
-import pandas as pd
+from utils.data_preprocessing import DataCleaning, DataReshaping
+from mlClassifier.dependencies.logistic_model_preprocessing import ModelBuilding, PredictionService
 
 """
 ## creates an instance of the preprocessing class, which imports data and drops null values.
 ## assigns each clean sample to their respective dataframes.
 """
 
-first_instance = data_preprocessing.DataPreProcessing('train_sample.csv', 'test_sample.csv')
-first_instance.import_data()
-[train_sample, test_sample] = first_instance.remove_null_values()
+features_to_use = ['Sha256', 'isPeFile', 'isValidSignedFile', 'fileSize', 'filePrevalence',
+                   'GeoId', 'Extension', 'ImportFunctionCount', 'ImportModuleCount',
+                   'PeAppendedSize', 'PeHeaderChecksum', 'PeTimestamp', 'FirstObserved']
 
-# Here goes the encoding and normalising data.
-
-"""
-## each dataset should have the same features (same name references).
-"""
-
-second_instance = data_preprocessing.FeatureSelection(train_sample, test_sample)
-[train_sample, test_sample] = second_instance.select_features()
-
-train_sample = pd.DataFrame(train_sample)
-test_sample = pd.DataFrame(test_sample)
+data_cleaning = DataCleaning('train_sample.csv', 'test_sample.csv')
+data_cleaning.import_data()
+[train_sample, test_sample] = data_cleaning.remove_null_values()
+data_reshape = DataReshaping(train_sample, test_sample, features_to_use)
+data_reshape.select_features()
+target = data_reshape.get_target_variable()
+[train_sample, test_sample] = data_reshape.force_dataframe_resize(target)
+predictions = PredictionService()
 
 """
-## once having the data then we need to get the model built.
+## Adding back the target data.
 """
 
-third_instance = ModelPreProcessing(train_sample, test_sample)
-third_instance.force_equal_split()
-x_train, x_test, y_train, y_test = third_instance.data_preparation()
-
-fourth_instance = ModelScalingAndEncoding(x_train, x_test, y_train, y_test)
-fourth_instance.feature_scaling()  # defines, selects and scales a variable if needed.
-fourth_instance.feature_encoding()  # defines, selects and encodes text variables in the sample.
-# Encode required text data, either binary or numerical.
-
-"""
-## Notes to self:
-## resizing using the array method does not work properly. Removes column labels and other relevant data.
-"""
+train_sample['label'] = target
+train_sample.to_csv('example_data.csv')
--- utils/data_preprocessing.py
+++ utils/data_preprocessing.py
@@ -1,72 +1,74 @@
 import pandas as pd
-import numpy as np
 from sklearn.preprocessing import LabelEncoder, StandardScaler
 
 
-class DataPreProcessing:
+class DataCleaning:
+
+    """
+    ## Class constructor
+    """
+
     def __init__(self, train_sample_name: str, test_sample_name: str):
         self.train = train_sample_name
         self.test = test_sample_name
+        self.train_label = train_sample_name
+        self.test_label = test_sample_name
+
+    """
+    ## currently importing data from csv and dropping NA values in both datasets.
+    """
 
     def import_data(self):
         self.train = pd.DataFrame(pd.read_csv(self.train, delimiter=';'))
         self.test = pd.DataFrame(pd.read_csv(self.test, delimiter=';'))
 
     def remove_null_values(self):
-        self.train = self.train.dropna()
-        self.test = self.test.dropna()
+        self.train = pd.DataFrame(self.train).dropna()
+        self.test = pd.DataFrame(self.test).dropna()
 
         return [self.train, self.test]
 
-    def encode_normalise(self):
+    """
+    ## Encoding text variables using a simple labeler encoder.
+    ## Normalising variables using standard scaler z = x-mean/ste.dev.
+    Not recommended with heavy outliers in the dataset.
+    """
+
+    def encode_normalise(self, training_sample, testing_sample):
         pass
 
 
-class FeatureSelection:
-    def __init__(self, sample_1, sample2):
+class DataReshaping:
+
+    """
+    ## Constructor selecting the features vector excluding the target.
+    ## Pending target reshaping.
+    """
+    def __init__(self, sample_1, sample_2, features_vector: list):
         self.x1 = pd.DataFrame(sample_1)
-        self.x2 = pd.DataFrame(sample2)
-        self.x1x2_labels = list(sample_1.columns)
-        self.num_items = len(self.x1x2_labels)
+        self.x2 = pd.DataFrame(sample_2)
+        self.variables_vector = features_vector
+
+    def get_target_variable(self):
+        max_columns: int = len(self.x1.columns) - 1
+        target = self.x1.pop(self.x1.columns[max_columns])
+
+        if self.x1.shape[0] > self.x2.shape[0]:
+            max_rows: int = self.x2.shape[0]
+            target = pd.DataFrame(target).iloc[0:max_rows]
+            return target
 
     def select_features(self):
-        selected_labels = []
+        return [self.x1[self.variables_vector], self.x2[self.variables_vector]]
 
-        for i in range(0, self.num_items):
-            selected = input('Do you need the feature ' + str(self.x1x2_labels[i]) + '? (y/n):')
+    def force_dataframe_resize(self, target):
 
-            if selected == 'y':
-                selected_labels.append(str(self.x1x2_labels[i]))
+        if self.x1.shape[0] < self.x2.shape[0]:
+            max_rows: int = self.x1.shape[0]
+            self.x2 = pd.DataFrame(self.x2.iloc[0:max_rows])
+            return [self.x1, self.x2]
 
-        try:
-            self.x2 = self.x2[selected_labels]
-
-        except:
-            self.x2 = self.x2[selected_labels[: len(selected_labels) - 1]]
-
-        return [self.x1[selected_labels], self.x2]
-
-    def force_dataframe_resize(self):
-        reset_dataframe = pd.DataFrame()
-
-        for sample in range(0, 2):
-
-            if sample == 1:
-                new_shape = (self.x2.shape[0], self.x1.shape[0])
-            else:
-                new_shape = (self.x2.shape[0], self.x1.shape[0])
-
-            for x_feature in self.x1x2_labels:
-                if sample == 1:
-                    current_feature = np.array(self.x1.pop(x_feature))
-                    current_feature.resize(new_shape)
-                    reset_dataframe.append(pd.DataFrame(current_feature))
-                    self.x1 = reset_dataframe
-                    return self.x1
-
-                else:
-                    current_feature = np.array(self.x2.pop(x_feature))
-                    current_feature.resize(new_shape)
-                    reset_dataframe.append(pd.DataFrame(current_feature))
-                    self.x2 = reset_dataframe
-                    return self.x2
+        else:
+            max_rows: int = self.x2.shape[0]
+            self.x1 = pd.DataFrame(self.x1.iloc[0:max_rows])
+            return [self.x1, self.x2]
