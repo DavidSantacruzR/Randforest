0c555f7c93dae4d1190dc0577ae9532e4c1f5b67
David Santacruz <axelsantacruzr@gmail.com>
2022-04-24 19:14:10 -0500

    Fix: typo in main file

--- main.py
+++ main.py
@@ -9,5 +9,5 @@ def welcome():
     return {"Hello": "There"}
 
 
-if __name__ == "__main":
-    uvicorn.run("main:classifier", host="127.0.0.1", port=3000, log_level="info")
+if __name__ == "__main__":
+    uvicorn.run("main:classifier", host="127.0.0.1", port=8000, log_level="info")
