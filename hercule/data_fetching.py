d92d0c849aa5a7a7a3d6760e1cba58cfba153481
DavidSantacruzR <axelsantacruzr@gmail.com>
2022-06-26 20:41:39 -0500

    feat: new project format using TDD

---
+++ hercule/data_fetching.py
@@ -0,0 +1,54 @@
+class Database:
+
+    def __init__(self, session, sample_size: float, table:str):
+        self.session = session()
+        self.size = sample_size
+        self.table = table
+        self.total_sample = self.fetch_number_of_records()
+        self.features = self.fetch_number_of_features()
+
+    def fetch_number_of_records(self) -> int:
+        pass
+
+    def fetch_number_of_features(self) -> int:
+        pass
+
+    def query_data(self):
+        pass
+
+    def __call__(self):
+        self.fetch_number_of_records()
+        self.fetch_number_of_features()
+
+        return self.query_data()
+
+
+class SQL(Database):  
+
+    def fetch_number_of_records(self) -> int:
+        return self.session.execute(
+            f'''
+                SELECT count(*) FROM {self.table}
+            '''
+            ).fetchone()[0]
+
+    def fetch_number_of_features(self) -> int:
+        return self.session.execute(
+            f'''
+                SELECT COUNT(*) FROM information_schema.columns
+                WHERE table_name = '{self.table}'
+            '''
+        ).fetchone()[0]
+
+    def query_data(self):
+        print(self.table)
+        return self.session.execute(
+            f'''
+                SELECT * FROM {self.table}
+                limit({self.total_sample * self.size})
+            '''
+            ).fetchall()
+
+
+class MongoDB(Database):
+    pass
